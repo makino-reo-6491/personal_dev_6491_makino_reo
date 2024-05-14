a9b8e83f55810567bba0de730b4ee26711bb4c91
makino-reo-6491 <420-student004@la-study.com>
2024-05-14 14:17:09 +0900

    最初のコミット

---
+++ src/test/java/com/example/demo/PersonalDev6491MakinoReoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDev6491MakinoReoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
